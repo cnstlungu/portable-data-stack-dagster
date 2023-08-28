e247489cabd69feaa25858842e3bf93405c0f000
Constantin Lungu <lungupconstantin@gmail.com>
2023-08-28 23:21:40 +0300

    Add versions to packages, upgrade duckdb version

--- dagster-dbt/setup.py
+++ dagster-dbt/setup.py
@@ -4,13 +4,13 @@ setup(
     name="postcard_company_dm",
     packages=find_packages(),
     install_requires=[
-        "dagster",
-        "dagster-dbt",
-        "polars",
-        "dbt-core",
-        "dbt-duckdb",
-        "dagster-duckdb",
-        "dagster-duckdb-polars"
+        "dagster==1.4.10",
+        "dagster-dbt==0.20.10",
+        "polars==0.18.15",
+        "dbt-core==1.4.7",
+        "dbt-duckdb==1.4.1",
+        "dagster-duckdb==0.20.10",
+        "dagster-duckdb-polars==0.20.10"
     ],
     extras_require={"dev": ["dagit", "pytest"]},
 )
