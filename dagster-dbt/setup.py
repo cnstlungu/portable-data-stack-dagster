a5d5da17e752c622ce3d638af756adbe932cec52
Colin Johnson <colinj2@gmail.com>
2024-02-05 20:51:46 -0600

    1. Upgrade to dagster 1.5.10 to fix issues with pendulum.  dagster 1.5.7 introduced new warnings that were fixed in 1.5.10. 2. Change to oltp service healthcheck to remove 'role "root" does not exist' Fatal logs. 3. Use `dagster-webserver` instead of the deprecated `dagit` cli command.

--- dagster-dbt/setup.py
+++ dagster-dbt/setup.py
@@ -4,12 +4,12 @@ setup(
     name="postcard_company_dm",
     packages=find_packages(),
     install_requires=[
-        "dagster==1.4.10",
-        "dagster-dbt==0.20.10",
+        "dagster==1.5.10",
+        "dagster-dbt==0.21.10",
         "duckdb==0.8.1",
-        "dbt-core==1.4.7",
-        "dbt-duckdb==1.4.1",
-        "dagster-duckdb==0.20.10"
+        "dbt-core==1.7.7",
+        "dbt-duckdb==1.7.1",
+        "dagster-duckdb==0.21.10"
     ],
     extras_require={"dev": ["dagit", "pytest"]},
 )
